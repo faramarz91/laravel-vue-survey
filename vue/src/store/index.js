cb96f74f70d8f87c8a1130690c4ae983d68d7014
faramarz <faramarz.mehrnami@eltturkey.com>
2022-06-15 13:54:03 +0300

    add store to ./vue

---
+++ vue/src/store/index.js
@@ -0,0 +1,17 @@
+import {createStore} from "vuex";
+
+
+const store = createStore({
+  state: {
+    user: {
+      data: {name: 'faramarz'},
+      token: null
+    }
+  },
+  getters: {},
+  action: {},
+  modules: {},
+  mutations: {}
+});
+
+export default store;
