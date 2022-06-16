b13764b3c025e3025f8995a60edce323c123fb70
faramarz <faramarz.mehrnami@eltturkey.com>
2022-06-16 16:22:04 +0300

    move login and register to AuthLayout.vue add register & login routes in router/index.js

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,7 +3,9 @@ import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
 import Survey from "../views/Survey.vue";
+import store from "../store";
 
 const routes = [
   {
@@ -11,21 +13,22 @@ const routes = [
     redirect: '/dashboard',
     name: 'Dashboard',
     component: DefaultLayout,
+    meta: {requiresAuth: true},
     children: [
       {path: '/dashboard', name: 'Dashboard', component: Dashboard},
       {path: '/surveys', name: 'Survey', component: Survey}
     ]
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: '/auth',
+    name: 'Auth',
+    redirect: '/login',
+    component: AuthLayout,
+    children: [
+      {path: '/login', name: 'Login', component: Login},
+      {path: '/register', name: 'Register', component: Register,}
+    ]
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register,
-  }
 
 ];
 
@@ -33,6 +36,17 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({name: 'Login'})
+  } else if (store.state.user.token && (to.name ==='Login' || to.name === 'Register')) {
+    console.log(to.name)
+    next({name: 'Dashboard'});
+  } else {
+    next();
+  }
 });
 
 export default router;
